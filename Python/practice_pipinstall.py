84f56ab52b851768dee2512d18f40b30876dd9f9
sungeu094 <sungeu094@gmail.com>
2025-08-03 00:18:44 +0900

    20250802-3

--- Python/practice_pipinstall.py
+++ Python/practice_pipinstall.py
@@ -2,7 +2,8 @@
 
 from bs4 import BeautifulSoup
 
-soup = BeautifulSoup("<p>Some<b>bad<i>HTML", features="html.parser")
+soup = BeautifulSoup("<p>Some<b>bad<i>HTML", features="html.parser")    
+# BeautifulSoup은 여러 parser(Ex. lxml, html.parser ..)를 지원하는데 그중에서 명확하게 html.parser를 지원한다는 것을 정해주기 위해서 features 키워드 인자를 사용한다. 
 print(soup.prettify())
 
 # pip list
